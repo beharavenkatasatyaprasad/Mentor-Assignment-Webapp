05c49a2065f39facf2d55612b7eade53d23ef8f8
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2020-11-23 07:51:17 +0530

    commit

--- js/common.js
+++ js/common.js
@@ -19,21 +19,55 @@ document.getElementById('ListMentorsBtn').addEventListener('click',()=>{
 
 
 function custom_alert(type, message) {
-    let newAlert = $("#message");
-    newAlert.html(`
-    <div class="fade-in text-center m-0 alert alert-${type} alert-dismissible fade show" role="alert">
-    ${message}
-    <button type="button" class=" close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-    </div>`);
-    $("html, body").animate(
-      {
-        scrollTop: $("#message").offset().top,
-      },
-      500
-    );
-    setTimeout(() => {
-      newAlert.html("");
-    }, 3000);
+  let newAlert = $("#message");
+  newAlert.html(`
+  <div class="fade-in text-center m-0 alert alert-${type} alert-dismissible fade show" role="alert">
+  ${message}
+  <button type="button" class=" close" data-dismiss="alert" aria-label="Close">
+     <span aria-hidden="true">&times;</span>
+  </button>
+  </div>`);
+  $("html, body").animate(
+    {
+    scrollTop: $("#message").offset().top,
+    },
+    500
+  );
+  setTimeout(() => {
+    newAlert.html("");
+  }, 3000);
+}
+
+if ($(window).width() > 350) {
+    $(window).scroll(function(){  
+  if ($(this).scrollTop() > 40) {
+     $('#navbar_top').addClass("fixed-top");
+     $('#navbar_top').addClass("fade-in");
+    // add padding top to show content behind navbar
+    $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
   }
+  else{
+    $('#navbar_top').removeClass("fixed-top");
+    $('#navbar_top').removeClass("fade-in");	      
+    // remove padding top from body
+    $('body').css('padding-top', '0');
+    }   
+  });
+} 
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+        
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    } 
+    else {
+       mybutton.style.display = "none";
+   }
+ }
+
+ function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
